dff24c3a4369dc86ce6fc39b076e11de7dbf6b91
Elvis Dukaj <elvis.dukaj@gmail.com>
2025-01-03 11:36:43 +0100

    Adding clang-tidy

--- conanfile.py
+++ conanfile.py
@@ -3,14 +3,30 @@ from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
 
 class StdexecTestPackage(ConanFile):
   settings = "os", "arch", "compiler", "build_type"
+  options = {
+          "enable_static_analyzer": [True, False]
+          }
+  default_options = {
+          "enable_static_analyzer": False
+          }
 
   def requirements(self):
     self.requires("stdnet/0.1.0")
     self.requires("fmt/11.0.2")
 
+  def layout(self):
+    self.folders.build_folder_vars = [
+            "settings.os",
+            "settings.arch",
+            "settings.compiler",
+            "settings.compiler.version"
+            ]
+    cmake_layout(self)
+
   def generate(self):
     tc = CMakeToolchain(self)
-    tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
+    tc.cache_variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = True
+    tc.cache_variables["ENABLE_STATIC_ANALYZER"] = self.options.enable_static_analyzer
     tc.generate()
 
     deps = CMakeDeps(self)
@@ -21,7 +37,6 @@ class StdexecTestPackage(ConanFile):
     cmake.configure()
     cmake.build()
 
-  def layout(self):
-    self.folders.build_folder_vars = [                                                                "settings.os",                                                                                "settings.arch",                                                                              "settings.compiler",                                                                          "settings.compiler.version"                                                               ]
-    cmake_layout(self)
+  def package_id(self):
+    self.info.options.rm_safe("enable_static_analyzer")
 
